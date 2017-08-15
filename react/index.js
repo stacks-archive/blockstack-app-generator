cdd143023b4e9dadf6e8cefaa5909188afe7a933
estermer <eric.stermer@gmail.com>
2017-08-15 15:47:29 -0400

    removed server from yoeman generator file

--- react/index.js
+++ react/index.js
@@ -56,12 +56,6 @@ module.exports = Generator.extend({
         this.destinationPath('src/components/Signin.jsx')
       );
     },
-    server: function () {
-      this.fs.copy(
-        this.templatePath('server.js'),
-        this.destinationPath('server.js')
-      );
-    },
     styles: function () {
       this.fs.copy(
         this.templatePath('src/styles/style.css'),
