cfb252be2494f0a279c945badd97064dd465b91f
Siddhant Goel <me@sgoel.org>
2018-11-14 17:52:43 +0100

    remove hardcoded port in opn URL

--- app/templates/server.js
+++ app/templates/server.js
@@ -1,7 +1,7 @@
-const express = require('express')  
+const express = require('express')
 const opn = require('opn')
 
-const app = express()  
+const app = express()
 const port = 5000
 
 function allowCrossDomain(req, res, next) {
@@ -13,10 +13,10 @@ function allowCrossDomain(req, res, next) {
 
 app.use(allowCrossDomain)
 app.use('/', express.static(__dirname + '/public'))
-app.listen(port, (err) => {  
+app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
   console.log(`server is listening on ${port}`)
-  opn('http://localhost:5000')
+  opn(`http://localhost:${port}`)
 })
