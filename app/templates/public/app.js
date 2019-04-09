3eeb20bcc67d70bc804001bc5abb28f600bee2f6
thinkverse <ctuvedsson@gmail.com>
2019-04-09 03:18:52 +0200

    Updated app from Blockstack@18 to Blockstack@19

--- app/templates/public/app.js
+++ app/templates/public/app.js
@@ -1,15 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById('signin-button').addEventListener('click', function(event) {
+document.addEventListener("DOMContentLoaded", event => {
+  let appConfig = {
+    scopes: [],
+    appDomain: window.location.origin,
+    redirectPath: '',
+    manifestPath: '/manifest.json',
+    coreNode: null,
+    authenticatorURL: 'https://browser.blockstack.org/'
+  }
+
+  let UserSession = new blockstack.UserSession(appConfig)
+
+  document.getElementById('signin-button').addEventListener('click', event => {
     event.preventDefault()
-    blockstack.redirectToSignIn()
-  })
-  document.getElementById('signout-button').addEventListener('click', function(event) {
-    event.preventDefault()
-    blockstack.signUserOut(window.location.href)
+    UserSession.redirectToSignIn()
   })
 
-  function showProfile(profile) {
-    var person = new blockstack.Person(profile)
+  document.getElementById('signout-button').addEventListener('click', event => {
+    event.preventDefault()
+    UserSession.signUserOut()
+    window.location = window.location.origin
+  })
+
+  function showProfile (profile) {
+    let person = new blockstack.Person(profile)
     document.getElementById('heading-name').innerHTML = person.name() ? person.name() : "Nameless Person"
     if(person.avatarUrl()) {
       document.getElementById('avatar-image').setAttribute('src', person.avatarUrl())
@@ -18,11 +31,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('section-2').style.display = 'block'
   }
 
-  if (blockstack.isUserSignedIn()) {
-    var profile = blockstack.loadUserData().profile
-      showProfile(profile)
-  } else if (blockstack.isSignInPending()) {
-    blockstack.handlePendingSignIn().then(function(userData) {
+  if (UserSession.isUserSignedIn()) {
+    const { profile } = UserSession.loadUserData()
+    showProfile(profile)
+  } else if (UserSession.isSignInPending()) {
+    UserSession.handlePendingSignIn().then(userData => {
       window.location = window.location.origin
     })
   }
